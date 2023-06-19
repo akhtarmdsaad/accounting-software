bfc89c7e43256b68601a8fc4db3f793d04206ba0
akhtarmdsaad <akhtarmdsaad@gmail.com>
2023-06-19 08:22:32 +0530

    minor fixes

--- mydukaan/views.py
+++ mydukaan/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("mudukaan homepage")
+    return render(request,"base.html")
